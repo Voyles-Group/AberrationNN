5ecc76bd4a9537d45ab2f861181ff4b3ffb833f3
wdwzyyg <jwei74@wisc.edu>
2023-06-28 11:49:23 -0500

    minor change

--- AberrationNN/__init__.py
+++ AberrationNN/__init__.py
@@ -1,3 +1,3 @@
 from AberrationNN import NestedUNet, train, dataset, blocks
 
-__all__ = ['dataset', 'train', 'NestedUNet', 'blocks', 'cwt_2d', 'mother_wavelets']
+__all__ = ['dataset', 'train', 'NestedUNet', 'blocks', 'cwt_2d', 'mother_wavelets', 'FFTResNet']
