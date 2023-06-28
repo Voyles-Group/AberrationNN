4299b787f9a3c38711bba63d1638122620521aa5
Jingrui Wei <jwei74@wisc.edu>
2023-06-28 11:48:45 -0500

    minor change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AberrationNN',
-    version='0.1.0',
+    # version='0.1.0',
     packages=find_packages(),
     url='https://github.com/wdwzyyg/AberrationNN.git',
     license='MIT',
